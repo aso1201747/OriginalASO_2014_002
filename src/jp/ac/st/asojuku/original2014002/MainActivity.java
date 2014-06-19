a482ffc8db68b004e6bf8a893e5906ddb9f5fe7a
aso1201747 <1201747@st.asojuku.ac.jp>
2014-06-19 14:27:02 +0900

    変更だよーーーーーーーーーーーーーーーーー

--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,21 +1,17 @@
 package jp.ac.st.asojuku.original2014002;
 
-
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
-public class MainActivity extends Activity  implements View.OnClickListener{
+public class MainActivity extends Activity implements
+View.OnClickListener {
 
 	SQLiteDatabase sdb = null;
 	MySQLiteOpenHelper helper = null;
@@ -26,32 +22,70 @@ public class MainActivity extends Activity  implements View.OnClickListener{
 		setContentView(R.layout.activity_main);
 	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
 
+		Intent vIntent = null;
+		switch(v.getId()){
+		case R.id.btn:  //メンテボタンが押された
+			vIntent = new Intent(MainActivity.this,MaintenanceActivity.class);
+			startActivity(vIntent);
+			break;
 
+		case R.id.btntou:  //登録ボタンを押された
+			//エディットテキストから入力内容を取り出す
+			EditText etv = (EditText)findViewById(R.id.edtMsg);
+			String inputMsg = etv.getText().toString();
+
+			if(inputMsg!=null && !inputMsg.isEmpty()){
+				helper.insertHitokoto(sdb, inputMsg);
+			}
+
+			//入力欄をクリア
+			etv.setText("");
+			break;
+
+		case R.id.btnch:  //一言チェックが押された
+
+			//MySQLiteOpenHelperのセレクト一言メソッドを呼び出して一言をランダムに取得
+			String strHitokoto = helper.selectRandomHitokoto(sdb);
+			//エディットテキストから入力内容を取り出す
+			vIntent = new Intent(this,HitokotoActivity.class);
+			//インテントに一言を混入
+			vIntent.putExtra("hitokoto", strHitokoto);
+			startActivity(vIntent);
+			break;
+			}
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onStart();
+	}
 
 	@Override
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
-		Button btnch = (Button)findViewById(R.id.btnch);
-		btnch.setOnClickListener(this);
-
 		Button btn = (Button)findViewById(R.id.btn);
 		btn.setOnClickListener(this);
-
 
 		Button btntou = (Button)findViewById(R.id.btntou);
 		btntou.setOnClickListener(this);
 
+		Button btnch = (Button)findViewById(R.id.btnch);
+		btnch.setOnClickListener(this);
 
-		if(sdb == null) {
+		if(sdb == null){
 			helper = new MySQLiteOpenHelper(getApplicationContext());
 		}
 		try{
 			sdb = helper.getWritableDatabase();
 		}catch(SQLiteException e){
-			Log.e("ERROR", e.toString());
-			// 異常終了
+			//異常終了
+			return;
 		}
 	}
 
@@ -63,52 +97,5 @@ public class MainActivity extends Activity  implements View.OnClickListener{
 		return true;
 	}
 
-
-
-	public void onClick(View v) {
-		int aaa = 0;
-		// TODO 自動生成されたメソッド・スタブ
-		switch(v.getId()){ //どのボタンが押されたか判定
-			case R.id.btnch: //btnMsgが押された
-				aaa = 2;
-
-				break;
-			case R.id.btn:
-				aaa = 1;
-				break;
-
-			case R.id.btntou:
-				EditText etv = (EditText)findViewById(R.id.edtMsg);
-				String inputMsg = etv.getText().toString();
-
-
-				if(inputMsg!=null && !inputMsg.isEmpty()){
-					helper.insertHitokoto(sdb, inputMsg);
-				}
-
-				etv.setText("");
-				break;
-
-		}
-		// 生成して代入用のIntentインスタンス変数を用意
-		Intent intent = null;
-		// ranの値によって処理をわける
-		switch(aaa){
-			case 2:
-				String strHitokoto = helper.selectRamdomHitokoto(sdb);
-				intent = new Intent(MainActivity.this, HitokotoActivity.class);
-
-				intent.putExtra("hitokoto", strHitokoto);
-				// 次画面のアクティビティ起動
-				startActivity(intent);
-				break;
-			case 1:
-				intent = new Intent(MainActivity.this, MaintenanceActivity.class);
-
-				startActivity(intent);
-				break;
-
-		}
-	}
-
 }
+
--- src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -1,6 +1,5 @@
 package jp.ac.st.asojuku.original2014002;
 
-
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteCursor;
@@ -10,15 +9,16 @@ import android.util.Log;
 
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 
-	 /**
-	  * @param context　呼び出しコンテクスト
-	  * @param name 利用DB名
-	  * @param factory カーソルファクトリー
-	  * @param version DBバージョン
-	  */
-	public MySQLiteOpenHelper(Context context) {
+	/**
+	 * @param context 呼び出しコンテクスト
+	 * @param name 利用DB名
+	 * @param factory カーソルファクトリー
+	 * @param version DBバージョン
+	 */
 
-		super(context, "20140021201747.sqlte3", null, 1);
+	public MySQLiteOpenHelper(Context context){
+
+		super(context, "20140021201747.sqlite3", null, 1);
 
 	}
 
@@ -26,70 +26,119 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		// TODO 自動生成されたメソッド・スタブ
 		db.execSQL("CREATE TABLE IF NOT EXISTS " +
-				"Hitokoto ( _id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , phrase TEXT)");
+				"Hitokoto ( _id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , phrase TEXT )");
+
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO 自動生成されたメソッド・スタブ
-		db.execSQL("drop table Hitokoto");
+		db.execSQL("drop table Hitokoto ");
 		onCreate(db);
 	}
 
 	/**
-	 * 引数のフレーズをHitokotoテーブルにインサートするプライベートメソッド
-	 * @param SQLiteDatebase インサート先DBのインスタンス変数
-	 * @param inputMsg インサートするメッセージ
+	 *  引数のフレーズをHitokotoテーブルにインサートするプライベートメソッド
+	 *  @param SQLiteDatsbase インサート先DBのインスタンス変数
+	 *  @param inputMsg インサートするメッセージ
 	 */
+
 	public void insertHitokoto(SQLiteDatabase db, String inputMsg){
 
-		String sqlstr =" insert into Hitokoto (phrase) values('" + inputMsg + "');";
-			try {
-
-				if(db==null){
-					Log.e("ERROR", "dbがnullだ！");
+		String sqlstr = "insert into Hitokoto (phrase) values('" + inputMsg + "');";
+				try{
+					//トランザクション開始
+					db.beginTransaction();
+					db.execSQL(sqlstr);
+					// トランザクション成功
+					db.setTransactionSuccessful();
+				}catch (SQLException e){
+					Log.e("ERROR", e.toString());
+				}finally{
+					//トランザクション終了
+					db.endTransaction();
 				}
-				// トランザクション開始
-				db.beginTransaction();
-				db.execSQL(sqlstr);
-				// トランザクション成功
-				db.setTransactionSuccessful();
-			} catch(SQLException e) {
-				Log.e("ERROR", e.toString());
-			} finally {
-				// トランザクション終了
-				db.endTransaction();
-			}
 		return;
+	}
+
+	/**
+	 *  引数のフレーズをHitokotoテーブルにインサートするプライベートメソッド
+	 *  @param SQLiteDatsbase インサート先DBのインスタンス変数
+	 *  @param inputMsg インサートするメッセージ
+	*/
+
+	public String selectRandomHitokoto(SQLiteDatabase db){
+
+		String rtString = null;
+
+		String sqlstr = "SELECT _id, phrase FROM Hitokoto ORDER BY RANDOM();";
+				try{
+					//トランザクション開始
+					SQLiteCursor cursor = (SQLiteCursor)db.rawQuery(sqlstr, null);
+					if(cursor.getCount()!=0){
+						//カーソル開始位置を先頭にする
+						cursor.moveToFirst();
+						rtString = cursor.getString(1);
+					}
+					cursor.close();
+				}catch (SQLException e){
+					Log.e("ERROR", e.toString());
+				}finally{
+					//すでにカーソルもcloseしてあるので、何もしない
+				}
+		return rtString;
+	}
+
+	/**
+	 * Hitokotoテーブルからデータをすべて取得
+	 * @param SQLiteDatabase SELECTアクセスするDBのインスタンス変数
+	 * @return 取得したデータの塊の票（導出表）のレコードをポイントするカーソル
+	 */
+
+	public SQLiteCursor selectHitokotoList(SQLiteDatabase db){
+
+		SQLiteCursor cursor = null;
+
+		String sqlstr = " SELECT _id, phrase FROM Hitokoto ORDER BY _id; ";
+		try{
+			// トランザクション開始
+			cursor = (SQLiteCursor)db.rawQuery(sqlstr, null);
+			if(cursor.getCount()!=0){
+				//カーソル開始位置を先頭にする
+			cursor.moveToFirst();
+		}
+		// cursorは呼び出し元へ返すからここではcloseしない
+		// cursor.close();
+
+	} catch (SQLException e){
+		Log.e("ERROR", e.toString());
+	}finally{
+
+	}
+	return cursor;
 	}
 
 
 	/**
-	 * 引数のフレーズをHitokotoテーブルにインサートするプライベートメソッド
-	 * @param SQLiteDatebase インサート先DBのインスタンス変数
-	 * @param inputMsg インサートするメッセージ
+	 * Hitokoto表から引数(id)で指定した値とカラム[_id]の値が等しいレコードを削除
+	 * @param SQLiteDatabase DELETEアクセスするDBのインスタンス変数
+	 * @param id カラム｢id｣と比較するために指定する削除条件の値
 	 */
-	public String selectRamdomHitokoto(SQLiteDatabase db){
 
-		String rtString =null;
+	public void deleteHitokoto(SQLiteDatabase db, int id){
 
-		String sqlstr =" SELECT _id, phrase FROM Hitokoto ORDER BY RANDOM();";
-			try {
-				// トランザクション開始
-				android.database.sqlite.SQLiteCursor cursor = (SQLiteCursor)db.rawQuery(sqlstr, null);
-				if(cursor.getCount()!=0){
-					// カーソル位置を先頭にする
-				cursor.moveToFirst();
-				rtString = cursor.getString(1);
-			}
-				cursor.close();
-			}	catch(SQLException e) {
-				Log.e("ERROR", e.toString());
-			} finally {
-				// 既にカーソルもcloseしてあるので、何もしない
-			}
-		return rtString;
-			}
-
-
+		String sqlstr = "DELETE FROM Hitokoto where _id = " + id + " ;";
+		try{
+			//トランザクション開始
+			db.beginTransaction();
+			db.execSQL(sqlstr);
+			//トランザクション成功
+			db.setTransactionSuccessful();
+		} catch (SQLException e){
+			Log.e("ERROR", e.toString());
+		}finally{
+			//トランザクション終了
+			db.endTransaction();
+		}
+	}
 }
